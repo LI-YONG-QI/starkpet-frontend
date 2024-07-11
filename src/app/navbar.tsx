38ccba7b631c5959440e677a6ba38e3dcfc7e46d
LI-YONG-QI <a2369028@gmail.com>
2024-07-11 18:52:37 +0800

    feat: connect modal

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,27 +1,38 @@
 "use client";
-import { useAccount, useConnect } from "@starknet-react/core";
+import { useAccount, useConnect, useDisconnect } from "@starknet-react/core";
 import { useStarknetkitConnectModal } from "starknetkit";
 
 export default function Navbar() {
-  const { connect, connectors } = useConnect();
+  const { connect, connectors, connectAsync } = useConnect();
+  const { disconnectAsync } = useDisconnect();
   const { address } = useAccount();
 
-  // const { starknetkitConnectModal } = useStarknetkitConnectModal({
-  //   connectors: connectors as any,
-  // });
+  const { starknetkitConnectModal } = useStarknetkitConnectModal({
+    connectors: connectors as any,
+  });
 
-  // const connectWallet = async () => {
-  //   try {
-  //     const { connector } = await starknetkitConnectModal();
-  //     await connect({ connector });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+  const connectWallet = async () => {
+    try {
+      const { connector } = await starknetkitConnectModal();
+      await connectAsync({ connector });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const disconnectWallet = async () => {
+    try {
+      await disconnectAsync();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="h-[100px]">
-      <ul>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      <button onClick={disconnectWallet}>Disconnect</button>
+      {/* <ul>
         {connectors.map((connector) => (
           <li key={connector.id}>
             <button onClick={() => connect({ connector })}>
@@ -29,7 +40,7 @@ export default function Navbar() {
             </button>
           </li>
         ))}
-      </ul>
+      </ul> */}
 
       <p>{address}</p>
     </div>
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -14,21 +14,21 @@ import { WebWalletConnector } from "starknetkit/webwallet";
 import { ArgentMobileConnector } from "starknetkit/argentMobile";
 
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
-  // const connectors = [
-  //   new InjectedConnector({ options: { id: "braavos", name: "Braavos" } }),
-  //   new InjectedConnector({ options: { id: "argentX", name: "Argent X" } }),
-  //   new WebWalletConnector({ url: "https://web.argent.xyz" }),
-  //   new ArgentMobileConnector(),
-  // ];
+  const connectors = [
+    new InjectedConnector({ options: { id: "braavos", name: "Braavos" } }),
+    new InjectedConnector({ options: { id: "argentX", name: "Argent X" } }),
+    new WebWalletConnector({ url: "https://web.argent.xyz" }),
+    new ArgentMobileConnector(),
+  ];
 
-  const { connectors } = useInjectedConnectors({
-    // Show these connectors if the user has no connector installed.
-    recommended: [argent()],
-    // Hide recommended connectors if the user has any connector installed.
-    includeRecommended: "always",
-    // Randomize the order of the connectors.
-    order: "random",
-  });
+  // const { connectors } = useInjectedConnectors({
+  //   // Show these connectors if the user has no connector installed.
+  //   recommended: [argent()],
+  //   // Hide recommended connectors if the user has any connector installed.
+  //   includeRecommended: "always",
+  //   // Randomize the order of the connectors.
+  //   order: "random",
+  // });
 
   return (
     <StarknetConfig
